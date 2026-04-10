7a2768f60d4a522b2c19429ea6eb556f145f0163
ashish sood <ashishsood1998@gmail.com>
2026-04-09 21:41:34 -0500

    class created for scores

--- assets/script/script.js
+++ assets/script/script.js
@@ -38,3 +38,19 @@ function shuffle(arr) {
 
     return newArray;
 }
+
+class Score {
+    #date;
+    #hits;
+    #percentage;
+
+    constructor(date, hits, percentage) {
+        this.#date = date;
+        this.#hits = hits;
+        this.#percentage = percentage;
+    }
+
+    getDate() { return this.#date; }
+    getHits() { return this.#hits; }
+    getPercentage() { return this.#percentage; }
+}
