8c070edcb63064e57a2a2b77029e343e09344594
Manvir kaur <manvirkaur8193@gmail.com>
2026-04-18 17:58:45 -0500

    function created display scores

--- assets/script/script.js
+++ assets/script/script.js
@@ -155,3 +155,22 @@ function endGame() {
     
     saveScore(score, percentage);
 }
+
+function displayScores() {
+
+    let scores = JSON.parse(localStorage.getItem("scores")) || [];
+
+    scoreCard.innerHTML = "";
+
+    for (let i = 0; i < scores.length; i++) {
+
+        let li = document.createElement("li");
+
+        li.textContent =
+            (i + 1) + ". " +
+            scores[i].hits + " words - " +
+            scores[i].percentage + "%";
+
+        scoreCard.appendChild(li);
+    }
+}
